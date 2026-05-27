1ab53b4b1d88bde958bcd5677aedfdd7a4a4fc69
devlucasaf <freitas.lucasaf@gmail.com>
2026-05-27 15:35:39 -0300

    refactor: atualizar imports para português em contasRemote.js

--- frontend/src/scripts/remotes/contas/contasRemote.js
+++ frontend/src/scripts/remotes/contas/contasRemote.js
@@ -1,44 +1,44 @@
-import { API_BASE_URL, getAuthHeaders, validateResponse } from '../remoteUtils.js';
+import { URL_BASE_API, obterCabecalhosAuth, validarResposta } from '../remoteUtils.js';
 
 const pesquisarContas = () => {
-    const url = `${API_BASE_URL}/contas`;
-    return fetch(url, { headers: getAuthHeaders() })
-        .then(validateResponse)
-        .then(response => response.json());
+    const url = `${URL_BASE_API}/contas`;
+    return fetch(url, { headers: obterCabecalhosAuth() })
+        .then(validarResposta)
+        .then(resposta => resposta.json());
 };
 
 const criarConta = (params = {}) => {
-    const url = `${API_BASE_URL}/contas`;
-    const options = {
+    const url = `${URL_BASE_API}/contas`;
+    const opcoes = {
         method: 'POST',
         body: JSON.stringify(params),
-        headers: getAuthHeaders(),
+        headers: obterCabecalhosAuth(),
     };
-    return fetch(url, options)
-        .then(validateResponse)
-        .then(response => response.json());
+    return fetch(url, opcoes)
+        .then(validarResposta)
+        .then(resposta => resposta.json());
 };
 
 const atualizarConta = (id, params = {}) => {
-    const url = `${API_BASE_URL}/contas/${id}`;
-    const options = {
+    const url = `${URL_BASE_API}/contas/${id}`;
+    const opcoes = {
         method: 'PUT',
         body: JSON.stringify(params),
-        headers: getAuthHeaders(),
+        headers: obterCabecalhosAuth(),
     };
-    return fetch(url, options)
-        .then(validateResponse)
-        .then(response => response.json());
+    return fetch(url, opcoes)
+        .then(validarResposta)
+        .then(resposta => resposta.json());
 };
 
 const excluirConta = (id) => {
-    const url = `${API_BASE_URL}/contas/${id}`;
-    const options = { 
-        method: 'DELETE', 
-        headers: getAuthHeaders() 
+    const url = `${URL_BASE_API}/contas/${id}`;
+    const opcoes = {
+        method: 'DELETE',
+        headers: obterCabecalhosAuth()
     };
-    return fetch(url, options)
-        .then(validateResponse)
+    return fetch(url, opcoes)
+        .then(validarResposta)
         .then(() => {});
 };
 
